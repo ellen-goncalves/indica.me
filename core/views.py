653009d6e8e13fde1d7b63d53c01b9f5f559f19a
ellen-goncalves <ellenctgoncalves@gmail.com>
2022-05-10 01:34:40 -0300

    add idea's list interface, authentication and restricted access, login and logout options, feature add new idea

--- core/views.py
+++ core/views.py
@@ -1,19 +1,69 @@
+from email.errors import MessageParseError
+from multiprocessing import context
+import re
 from django.http import HttpResponse
-from django.shortcuts import render
-from templates import *
-from core import models
+from django.shortcuts import redirect, render
+from core.models import Idea
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
-def hello (request, nome, idade):
-    return HttpResponse('Hello {} de {} anos'.format(nome, idade))
+def home (request):
+    ideas = Idea.objects.order_by('-created_on')[:3]
+    return render(request, 'home.html', {'ideas':ideas})
 
-def soma (request, numero1, numero2):
-    soma = numero1 + numero2
-    return HttpResponse('A soma é {}'.format(soma))
+def login_user(request):
+    return render(request, 'login.html')
 
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha incorretos.")
+            return redirect('/login')
+    else:
+        return redirect('/login')
+    
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+@login_required(login_url='/login/')
 def ideas (request):
-    return render('index.html')
+    user = request.user
+    ideas = Idea.objects.all()
+    context = {'ideas': ideas, 'bodyId':"page-ideas", 'author':user.username}
+    return render(request, 'ideias.html', context)
 
+@login_required(login_url='/login/')
+def minhas_ideas (request):
+    user = request.user
+    ideas = Idea.objects.filter(author=user)
+    context = {'ideas': ideas, 'bodyId':"page-ideas"}
+    return render(request, 'ideias.html', context)
+
+@login_required(login_url='/login/')
 def mostrar_idea (request, titulo_ideia):
-    return HttpResponse(idea.objects.get(title=titulo_ideia))
+    idea = Idea.objects.get(id=titulo_ideia)
+    context = {'idea': idea, 'bodyId':"page-ideas"}
+    return render(request, 'each_idea.html', context)
+
+@login_required(login_url='/login/')
+def adicionar_ideia(request):
+    if request.POST:
+        title = request.POST.get('title')
+        category = request.POST.get('category')
+        image = request.POST.get('image')
+        description = request.POST.get('description')
+        link = request.POST.get('link')
+        author = request.user
+        idea = Idea.objects.create(title=title, category=category, image=image, description=description, url=link, author=author)
+
+    return redirect('/')
